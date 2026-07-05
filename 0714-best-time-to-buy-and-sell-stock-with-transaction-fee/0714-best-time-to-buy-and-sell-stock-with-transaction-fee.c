6241237182276fe637f63088da81590ca5177a3d
Raj Vardhan Srivastava <vraj35415@gmail.com>
2026-07-05 18:10:34 +0530

    Time: 0 ms (100%), Space: 13 MB (93.48%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.c
@@ -0,0 +1,13 @@
+int maxProfit(int* prices, int pricesSize, int fee) {
+    int cash = 0;
+    int hold = -prices[0];
+
+    for (int i = 1; i < pricesSize; i++) {
+        int newCash = cash > (hold + prices[i] - fee) ? cash : (hold + prices[i] - fee);
+        int newHold = hold > (cash - prices[i]) ? hold : (cash - prices[i]);
+        cash = newCash;
+        hold = newHold;
+    }
+
+    return cash;
+}
