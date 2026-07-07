5399fa74c91115fc90180b6b574204dfff844c74
Raj Vardhan Srivastava <vraj35415@gmail.com>
2026-07-07 14:30:40 +0530

    Time: 1 ms (42.6%), Space: 14.5 MB (10.93%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.c
@@ -0,0 +1,21 @@
+int longestOnes(int* nums, int numsSize, int k) {
+    int left = 0;
+    int zeroCount = 0;
+    int maxLen = 0;
+    for (int right = 0; right < numsSize; right++) {
+        if (nums[right] == 0) {
+            zeroCount++;
+        }
+        while (zeroCount > k) {
+            if (nums[left] == 0) {
+                zeroCount--;
+            }
+            left++;
+        }
+        int windowLen = right - left + 1;
+        if (windowLen > maxLen) {
+            maxLen = windowLen;
+        }
+    }
+    return maxLen;
+}
